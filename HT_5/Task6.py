1370c463042c75d7e18625b0b3a544f25e3b2862
Alex Khomenko <alexkhomenko45@gmail.com>
2021-12-02 00:30:15 +0100

    task5

--- HT_5/Task6.py
+++ HT_5/Task6.py
@@ -4,4 +4,20 @@
    P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
 """
 
-def generator_func(number1,number2):
+def generator_func(start: int,stop=0,step=1):
+    if stop == 0:
+        stop = start
+        start = 0
+
+    while start < stop:
+        start += step
+        yield start
+
+for i in generator_func(5):
+  print(i)
+
+
+
+
+
+
