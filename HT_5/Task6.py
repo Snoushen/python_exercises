d1d0957652bf23a3783476585f5d9022e792019d
Alex Khomenko <alexkhomenko45@gmail.com>
2021-12-03 18:40:08 +0100

    update

--- HT_5/Task6.py
+++ HT_5/Task6.py
@@ -4,20 +4,27 @@
    P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній: https://docs.python.org/3/library/stdtypes.html#range
 """
 
-def generator_func(start: int,stop=0,step=1):
-    if stop == 0:
-        stop = start
-        start = 0
+def custom_range(stop,start=None,step=1):
+    if step == 0:
+        raise ValueError
+    if start != None:
+        stop, start = start, stop
 
-    while start < stop:
-        start += step
-        yield start
+    print(start," ",stop)
+    try:
+        if step > 0:
+            while start < stop:
+                start += step
+                yield start
+        if step < 0:
+            while start > stop:
+                start += step
+                yield start
 
-for i in generator_func(5):
-  print(i)
+    except ValueError:
+        pass
 
-
-
-
-
-
+print(list(custom_range(0,10)))
+print(list(custom_range(-10,10,2)))
+print(list(custom_range(-20,-10,2)))
+print(list(custom_range(10,0)))
