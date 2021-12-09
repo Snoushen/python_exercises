5d3b63f41bf8c4047e08b3a2b759ee38d2a244ec
Alex Khomenko <alexkhomenko45@gmail.com>
2021-12-09 21:08:35 +0100

    v2.0

--- HT_7/Task2/Task2.py
+++ HT_7/Task2/Task2.py
@@ -9,45 +9,49 @@
 символів відцентрувати щодо середини файла і взяти необхідну кількість. В разі необхідності заокруглення одного чи обох параметрів - дивіться на свій розсуд.
 """
 
+import os
+
+def middle(some_string):
+    middle_index = int((len(some_string) / 2))
+    for x in range(len(some_string)):
+        if x == middle_index:
+            print("^")
+        elif x < middle_index:
+            print(" ", end="")
+    return None
+
 def strange_fun(filename,char):
-    with open(f'{filename}.txt','r') as file:
+    cwd = os.getcwd()  # Get the current working directory (cwd)
+    files = os.listdir(cwd)
+
+    with open(f'{filename}','r') as file:
         data_all_symbol = file.read()
         without_space = data_all_symbol.replace(" ","")
 
         if char > len(without_space):
             raise ValueError(f'Number of symbol in file {len(without_space)}, request = {char}')
         my_string = without_space[:char]
+        middle_index = int((len(my_string) / 2))
 
-        middle_index = int((len(my_string) /2)-(3/2))
+        print(my_string)
+        middle(my_string)
 
-        middle2 = int(len(my_string) / 2)
-        print(my_string,end="")
-        print("\n", (middle2 - 1) * " " + "^")
         for i in range(len(my_string)):
-            if len(my_string) % 2 == 0:
-                if i <= 2:
-                    print(my_string[i],end="")
-                elif middle_index < i <= middle_index+3:
-                    print(my_string[i],end="")
-                elif i > len(my_string)-4:
-                    print(my_string[i],end="")
-                else:
-                    print(" ",end="")
-            if len(my_string) % 2 != 0:
-                if i <= 2:
-                    print(my_string[i], end="")
-                elif middle_index-1< i <= middle_index + 2:
-                    print(my_string[i], end="")
-                elif i > len(my_string) - 4:
-                    print(my_string[i], end="")
-                else:
-                    print(" ", end="")
-        print("\n",(middle2 - 1) * " " + "^")
+            if i <= 2:
+                print(my_string[i],end="")
+            elif middle_index - 2 < i <= middle_index+1:
+                print(my_string[i],end="")
+            elif i > len(my_string)-4:
+                print(my_string[i],end="")
+            else:
+                print(" ",end="")
+        print("")
+        middle(my_string)
 
-strange_fun("test1",12)
+
+
+strange_fun("test1.txt",1)
 print(30*"=")
-strange_fun("test2",25)
+strange_fun("test2.txt",25)
 print(30*"=")
-strange_fun("test3",3)
-
-
+strange_fun("test3.txt",3)
