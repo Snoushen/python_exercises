e544b668313080451d5a094f743465ffdaf4deb0
Alex Khomenko <alexkhomenko45@gmail.com>
2021-12-11 00:56:27 +0100

    v3

--- HT_7/Task2/Task2.py
+++ HT_7/Task2/Task2.py
@@ -7,47 +7,51 @@
    В репозиторій додайте і ті файли, по яким робили тести.
    Як визначати середину файлу (з якої брать необхідні символи) - кількість символів поділити навпіл, а отримане "вікно"
 символів відцентрувати щодо середини файла і взяти необхідну кількість. В разі необхідності заокруглення одного чи обох параметрів - дивіться на свій розсуд.
-"""
 
-def middle(some_string):
-    middle_index = int((len(some_string) / 2))
-    for x in range(len(some_string)):
-        if x == middle_index:
-            print("^")
-        elif x < middle_index:
-            print(" ", end="")
-    return None
+
+"""
+from pathlib import Path
 
 def strange_fun(filename,char):
 
-    with open(f'{filename}','r') as file:
+    def print_symbol(start, stop, string):
+        i = 0
+        block = ""
+        while i < len(string):
+            if i == start:
+                x = i + stop
+                while i < x:
+                    block += string[i]
+                    i += 1
+            i += 1
+
+        return block
+
+    outpath = Path.cwd() / filename
+
+    with open(outpath,'r') as file:
         data_all_symbol = file.read()
         without_space = data_all_symbol.replace(" ","")
 
         if char > len(without_space):
             raise ValueError(f'Number of symbol in file {len(without_space)}, request = {char}')
-        my_string = without_space[:char]
-        middle_index = int((len(my_string) / 2))
 
-        print(my_string)
-        middle(my_string)
+        initial_index_middle = int((len(without_space) / 2)-char/2)
 
-        for i in range(len(my_string)):
-            if i <= 2:
-                print(my_string[i],end="")
-            elif middle_index - 2 < i <= middle_index+1:
-                print(my_string[i],end="")
-            elif i > len(my_string)-4:
-                print(my_string[i],end="")
-            else:
-                print(" ",end="")
+        print(without_space,"\n")
+
+        first = print_symbol(0, char,without_space)
+        second = print_symbol(initial_index_middle, char, without_space)
+        third = print_symbol(len(without_space)-char, char,without_space)
+
+        result = [first,second,third]
+
         print("")
-        middle(my_string)
+        print(result)
+
+symbol_in_block = int(input("Symbol in one block: "))
+strange_fun("test1.txt",symbol_in_block)
 
 
 
-strange_fun("test1.txt",1)
-print(30*"=")
-strange_fun("test2.txt",25)
-print(30*"=")
-strange_fun("test3.txt",3)
+
